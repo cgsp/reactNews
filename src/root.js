2e0d9b1af6848e776b7635794d53070b082c81db
gsp <cgsp740178088@163.com>
2018-03-17 13:53:52 +0800

    '项目初始化'

--- src/root.js
+++ src/root.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppComponent from './main';
-import ListComponent from './list';
-import ListDetailComponent from './components/list-detail/list-detail';
-import { Router, Route, hashHistory } from 'react-router';
-
 export default class Root extends React.Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        {/* localhost:8080/ */}
-        <Route path="/" component={AppComponent} >
-          <Route path='listDetail' component={ListDetailComponent}></Route>
-        </Route>
-        {/* localhost:8080/list */}
-        <Route path='list' component={ListComponent}>
-          <Route path='listDetail/:id/:type' component={ListDetailComponent}></Route>
-        </Route>
-      </Router>
+      <div>init</div>
     );
   };
 }
 
-// 替换main.js。这个地方变为整个程序的入口
 ReactDOM.render(
-  <Root />, document.getElementById('example'));
+  <Root />, document.getElementById('root'));
